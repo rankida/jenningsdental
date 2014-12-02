9f5a3cc2818e42f31f30bd15842aed180acfa548
rankida <david.rankin@spartan-solutions.com>
2014-12-02 14:21:40 +0000

    Filled out the skeleton of the initializr/index page, which will become the main page shortly.

--- initializr/js/main.js
+++ initializr/js/main.js
@@ -1,1 +1,35 @@
+$(document).ready(function (){
 
+  // create a LatLng object containing the coordinate for the center of the map
+  var latlng = new google.maps.LatLng(55.941764, -4.317066);
+
+  // prepare the map properties
+  var options = {
+    zoom: 16,
+    center: latlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    navigationControl: true,
+    mapTypeControl: false,
+    scrollwheel: false,
+    disableDoubleClickZoom: true
+  };
+
+  // initialize the map object
+  var map = new google.maps.Map(document.getElementById('google_map'), options);
+
+  // add Marker
+  var marker1 = new google.maps.Marker({
+    position: latlng, map: map
+  });
+
+  // add listener for a click on the pin
+  google.maps.event.addListener(marker1, 'click', function() {
+    infowindow.open(map, marker1);
+  });
+
+  // add information window
+  var infowindow = new google.maps.InfoWindow({
+    content:  '<div class="info"><strong>Jennings Dental Care</strong><br>4 Station Road, Milngavie<br> G62 8AB</div>'
+  });  
+
+});
